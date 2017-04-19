fcce69bc179a5b30434d87c5070565827cf1172e
William Mosca <O659@O659.(none)>
2017-04-19 18:06:01 -0400

    Added database methods

--- data/games.js
+++ data/games.js
@@ -7,8 +7,38 @@ const gameCollection = mongoCollections.games;
 const uuidV1 = require('uuid/v1');
 
 let exportedMethods = {
-
     
+
+    getGameById(id)
+    {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
+
+        return gameCollection().then((games) => {
+            return games.findOne({ _id: id });
+        });
+    },
+
+    getGameByName(name) 
+    {
+        if (!name) 
+            return Promise.reject("You must provide a Video Game to search for");
+
+        return gameCollection().then((games) => {
+            return games.findOne({ name: name });
+        });
+    },
+    
+    getGamesByGenre(genre)
+    {
+        if (!genre)
+            return Promise.reject("You must provide a genre to search for");
+        
+        return gameCollection().then((games) => {
+            return games.find({ keywords: { $elemMatch: genre } });
+        });
+    },
+
 }
 
 module.exports = exportedMethods;
--- data/users.js
+++ data/users.js
@@ -8,6 +8,25 @@ const uuidV1 = require('uuid/v1');
 
 let exportedMethods = {
 
+    getUserById(id)
+    {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
+
+        return userCollection().then((users) => {
+            return users.findOne({ _id: id });
+        });
+    },
+
+    getUserProfileById(id)
+    {
+        return this.getUserById(id).then((user) => {
+            if (!user) 
+                return Promise.reject("User not found");
+
+            return user.profile;
+        });
+    }
     
 }
 

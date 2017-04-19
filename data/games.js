0ea23ed494b321db23a0f0f54961a4e0aed35a3f
William Mosca <O659@O659.(none)>
2017-04-19 17:59:57 -0400

    updated data

--- data/games.js
+++ data/games.js
@@ -8,7 +8,78 @@ const uuidV1 = require('uuid/v1');
 
 let exportedMethods = {
 
+    getGameById(id)
+    {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
+
+        return gameCollection().then((games) => {
+            return games.findOne({ _id: id });
+        });
+    },
+
+    getGameByName(name) 
+    {
+        if (!name) 
+            return Promise.reject("You must provide a Video Game to search for");
+
+        return gameCollection().then((games) => {
+            return games.findOne({ name: name });
+        });
+    },
     
+    getGamesByGenre(genre)
+    {
+        if (!genre)
+            return Promise.reject("You must provide a genre to search for");
+        
+        return gameCollection().then((games) => {
+            return games.find({ keywords: { $elemMatch: genre } });
+        });
+    },
+
+    getGamesByKeyword(key)
+    {
+        if (!key)
+            return Promise.reject("You must provide a keyword to search for");
+        
+        return gameCollection().then((games) => {
+            return games.find({$or:[
+                { name: key},
+                { keywords: { $elemMatch: key }}] });
+        });
+    },
+
+    addGame(gameData)
+    {
+        if (!gameData.name) 
+            return Promise.reject("Games require a name");
+        if (!gameData.description) 
+            return Promise.reject("Games require a description");
+        if (!gameData.art)
+            return Promise.reject("Games require art")
+        if (!gameData.keywords)
+            return Promise.reject("Games require keywords");
+        
+        return gameCollection().then((games) => {
+            let newGame = {     
+                _id: uuidV1(),
+                name: gameData.name,
+                keywords: gameData.keywords,
+                art: gameData.art,
+                description: description
+            };
+
+            return games
+                .insertOne(newGame)
+                .then((newInsertInformation) => {
+                    return newInsertInformation.insertedId;
+                })
+                .then((newId) => {
+                    return this.getGameById(newId);
+                });
+        });
+    }
 }
 
 module.exports = exportedMethods;
--- data/users.js
+++ data/users.js
@@ -6,9 +6,27 @@ const mongoCollections = require("../config/mongoCollections");
 const userCollection = mongoCollections.users;
 const uuidV1 = require('uuid/v1');
 
-let exportedMethods = {
+let exportedMethods = 
+{
+    getUserById(id)
+    {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
 
-    
+        return userCollection().then((users) => {
+            return users.findOne({ _id: id });
+        });
+    },
+
+    getUserProfileById(id)
+    {
+        return this.getUserById(id).then((user) => {
+            if (!user) 
+                return Promise.reject("User not found");
+
+            return user.profile;
+        });
+    }
 }
 
 module.exports = exportedMethods;

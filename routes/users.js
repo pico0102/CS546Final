d0ddbd46fc3801ef9da264f12d9ddf1f948d882f
Anthony Picone <anthony@host-studentw-135-157.dhcp.stevens-tech.edu>
2017-05-04 16:45:27 -0400

    added user routes

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,60 @@ const userData = data.users;
 const gameData = data.games;
 
 
-router.get("/", (req, res) => {
-    
+router.get("/:id", (req, res) => {
+    userData.getUserById(req.params.id).then((user) => {
+        res.json(user);
+    }).catch(() => {
+        res.status(404).json({ error: "User not found" });
+    });
+});
+
+router.post("/", (req, res) => {
+    let userInfo = req.body;
+
+    if (!userInfo) {
+        res.status(400).json({ error: "You must provide data to create a user" });
+        return;
+    }
+
+    if (!userInfo.password) {
+        res.status(400).json({ error: "You must provide a password" });
+        return;
+    }
+
+    if (!userInfo.name) {
+        res.status(400).json({ error: "You must provide name" });
+        return;
+    }
+
+    userData.addUser(userInfo.password, userInfo.name)
+        .then((newUser) => {
+            res.json(newUser);
+        }, () => {
+            res.sendStatus(500);
+        });
+});
+
+router.put("/:id", (req, res) => {
+    let userInfo = req.body;
+
+    if (!userInfo) {
+        res.status(400).json({ error: "You must provide data to update a recipe" });
+        return;
+    }
+
+    let getRecipe = userData.getUserById(req.params.id).then(() => {
+        return userData.updateUser(req.params.id, userInfo)
+            .then((updatedUser) => {
+                res.json(updatedUser);
+            }, (error) => {
+                console.log(error);
+                res.sendStatus(500);
+            });
+    }).catch(() => {
+        res.status(404).json({ error: "User not found" });
+    });
 
 });
+
+module.exports = router;

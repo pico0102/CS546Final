1c6dec0b1250e5b1a63f573509d2eb2edb01c3c5
William Mosca <O659@O659.(none)>
2017-05-04 17:01:40 -0400

    Added addUser

--- data/users.js
+++ data/users.js
@@ -8,6 +8,37 @@ const uuidV1 = require('uuid/v1');
 
 let exportedMethods = {
 
+    addUser(userData)
+    {
+        if(!userData.name)
+            return Promise.reject("User Profiles require a name");
+        if(!userData.password)
+            return Promise.reject("Users require a password"); 
+        
+        var userId = uuidV1();
+
+        return userCollection().then((users) => {
+            let newUser = {     
+                _id: userId,
+                password: password,
+                profile: {
+                    _id: userId,
+                    name: userData.name,
+                    games: []
+                }
+            };
+
+            return users
+                .insertOne(newUser)
+                .then((newInsertInformation) => {
+                    return newInsertInformation.insertedId;
+                })
+                .then((newId) => {
+                    return this.getUserById(newId);
+                });
+        });
+    },
+
     getUserById(id)
     {
         if (!id) 
